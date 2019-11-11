68f7230918a52718e82735f3ea92db31f10dfe79
faithforus <ljunf817@163.com>
2019-11-11 10:26:06 +0800

    add icon

--- ctpbee_desktop.py
+++ ctpbee_desktop.py
@@ -1,23 +1,32 @@
 import os
 import sys
 import platform
+
+from PySide2.QtGui import QIcon, QPixmap
 from PySide2.QtWidgets import QApplication
 
 from app.lib.get_path import init_file
 from app.main import MainWindow
 
 if __name__ == '__main__':
-    # if platform.system() == 'Windows':
-    #     import PySide2
-    #
-    #     dirname = os.path.dirname(PySide2.__file__)
-    #     plugin_path = os.path.join(dirname, 'plugins', 'platforms')
-    #     os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
+
+    if platform.system() == 'Windows':
+        import PySide2
+
+        dirname = os.path.dirname(PySide2.__file__)
+        plugin_path = os.path.join(dirname, 'plugins', 'platforms')
+        os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
+
+        import ctypes
+        ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID("myappid")
 
     init_file()
     app = QApplication(sys.argv)
 
     window = MainWindow()
+    icon = QIcon()
+    icon.addPixmap(QPixmap("bee2.ico"), QIcon.Normal, QIcon.Off)
+    window.setWindowIcon(icon)
     window.show()
 
     sys.exit(app.exec_())

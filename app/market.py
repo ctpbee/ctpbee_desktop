46e9383da594f2ea3b2bf928a05dbec9574b3e62
faithforus <ljunf817@163.com>
2019-12-30 12:13:47 +0800

    '#'

--- app/market.py
+++ app/market.py
@@ -56,6 +56,7 @@ class MarketWidget(QWidget, Ui_Market):
         self.tableWidget.setSelectionBehavior(QAbstractItemView.SelectRows)
         self.tableWidget.cellDoubleClicked.connect(self.cellDoubleClicked_slot)
         self.tableWidget.verticalScrollBar().installEventFilter(self)
+        self.tableWidget.verticalScrollBar().setSingleStep(1)
         # 渲染table
         self.obj = self.mainwindow.job
         self.obj.market_signal.connect(self.set_item_slot)
@@ -165,9 +166,9 @@ class MarketWidget(QWidget, Ui_Market):
 
     @Slot()
     def subscribe_slot(self):
-        text = self.symbol_list.currentText()
-        local_symbol = text.split(contract_space)[0]
-        name = text.split(contract_space)[1]
+        text = self.symbol_list.currentText().split(contract_space)
+        local_symbol = text[0]
+        name = text[1]
         if local_symbol not in G.all_contracts:
             TipDialog("未知合约")
             return
